a2277f1741253e5853d65004b13fa8b205d4afe6
Lucas Himelfarb <lucashimelfarb@infras-MacBook-Air.local>
2025-07-04 21:07:30 -0300

    fix: apply prettier

--- agents/eslint.config.mjs
+++ agents/eslint.config.mjs
@@ -1,16 +1,22 @@
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-import prettier from 'eslint-config-prettier';
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-export default defineConfig([
-  { files: ['**/*.js', '**/*.ts'], languageOptions: { sourceType: 'module' } },
-  {
-    ignores: ['node_modules', 'dist'],
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    languageOptions: { globals: globals.node },
-  },
-  tseslint.configs.recommended,
-  prettier,
-], eslintConfigPrettier);
+export default defineConfig(
+  [
+    {
+      files: ['**/*.js', '**/*.ts'],
+      languageOptions: { sourceType: 'module' },
+    },
+    {
+      ignores: ['node_modules', 'dist'],
+      files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+      languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+    prettier,
+  ],
+  eslintConfigPrettier
+)
